5f709ae6f0c517c181b1ed9f9b2ee9e1aa4c7456
przemek <wosin7@gmail.com>
2018-08-30 19:48:26 +0200

    poprawki

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -9,19 +9,17 @@ var ContactForm = React.createClass({
         <input 
             type={'text'}
             placeholder={'Imię'}
-            value={this.props.contact.firstName}
-        />
+            value={this.props.contact.firstName}/>
         <input 
             type={'text'}
             placeholder={'Nazwisko'}
-            value={this.props.contact.lastName}
-        />
+            value={this.props.contact.lastName}/>
         <input 
             type={'email'}
             placeholder={'Email'}
-            value={this.props.contact.email}
-        />
+            value={this.props.contact.email}/>
         <button type={'submit'}>Dodaj kontakt</button>
     </form>
+      )
     },
   }) 
